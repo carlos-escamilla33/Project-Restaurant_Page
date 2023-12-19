00cd4a03b167f9cb87163fb0417bc7fd73cad944
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2023-12-19 12:08:14 -0800

    working on setting up the menu

--- src/menu.js
+++ src/menu.js
@@ -2,29 +2,44 @@
 const pastries = [
     {
         name: "Macaroons",
-        img: "../media/macaroons.jpg"
+        imgURL: "../media/macaroons.jpg"
     },
     {
         name: "Croissant",
-        img: "../media/croissant.jpg"
+        imgURL: "../media/croissant.jpg"
     },
     {
         name: "Cinnamon Roll",
-        img: "../media/cinnamonroll.jpg"
+        imgURL: "../media/cinnamonroll.jpg"
     },
     {
         name: "Cheesecake",
-        img: "../media/cheesecake.jpg"
+        imgURL: "../media/cheesecake.jpg"
     },
     {
         name: "Scones",
-        img: "../media/scones.jpg"
+        imgURL: "../media/scones.jpg"
+    },
+    {
+        name: "Cupcake",
+        imgURL: "../media/cupcake.jpg"
     },
 ]
 
 const menu = () => {
     const menuDiv = document.createElement("div")
-    menuDiv.classList.add("default-styling")
+    // menuDiv.classList.add("default-styling")
+    menuDiv.classList.add("menu-styling")
+
+    for (let i = 0; i < pastries.length; i++) {
+        const image = document.createElement("img")
+        const pastry = pastries[i]
+
+        image.src = pastry.imgURL
+        image.alt = pastry.name
+
+        menuDiv.appendChild(image)
+    }
     
     return menuDiv
 }
