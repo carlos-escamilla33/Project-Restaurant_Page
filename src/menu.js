3b190d9fb5ebbec2fcda7ab37e43a68d53f4972e
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2023-12-19 09:13:56 -0800

    added macaroons jpg

---
+++ src/menu.js
@@ -0,0 +1,30 @@
+
+const pastries = [
+    {
+        name: "Macaroons",
+        img: ""
+    },
+    {
+        name: "Croissant",
+        img: ""
+    },
+    {
+        name: "Cinnamon Roll",
+        img: ""
+    },
+    {
+        name: "Cheesecake",
+        img: ""
+    },
+    {
+        name: "Scones",
+        img: ""
+    },
+
+]
+
+const menu = () => {
+
+}
+
+export default menu
