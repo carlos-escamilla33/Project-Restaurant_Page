4bbe00c38893e15f808510a9f1dc937558555845
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2023-12-18 16:28:01 -0800

    working on structure of homepage

--- src/home.js
+++ src/home.js
@@ -1,7 +1,18 @@
 
 const home = () => {
-    const homeDiv = document.createElement("div");
-    homeDiv.setAttribute("id", "home-container");
+    const homeDiv = document.createElement("div")
+    homeDiv.setAttribute("id", "home-container")
+
+    const menuDiv = document.createElement("div")
+    menuDiv.setAttribute("id", "menu")
+    homeDiv.appendChild(menuDiv)
+
+    const h1 = document.createElement("h1")
+    h1.textContent = "Pug & Puff"
+    homeDiv.appendChild(h1)
+
+
+    
 
 
 
